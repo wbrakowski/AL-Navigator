13d711581b5c898758658745357a0d9a0d495534
wbrakowski <wbrakowski@gmail.com>
2019-10-22 14:48:33 +0200

    Release 0.0.3

--- src/fileJumper.ts
+++ src/fileJumper.ts
@@ -1,6 +1,6 @@
 import { workspace, ExtensionContext, commands, window, Selection, Range, Position } from 'vscode';
 
-export module ReportJumper {
+export module FileJumper {
    export function jumpToNextDataItem() {
     let text : string = "DATAITEM(";
     jumpToNextTextOccurence(text);
@@ -11,6 +11,17 @@ export module ReportJumper {
     jumpToNextTextOccurence(text);
    }
 
+   export function jumpToKeys(){
+    let text : string = "KEYS";
+    jumpToNextTextOccurence(text);
+   }
+
+   export function jumpToNextTrigger(){
+    let text : string = "TRIGGER ON";
+    jumpToNextTextOccurence(text);
+   }
+
+
    function jumpToNextTextOccurence(text: string)
    {
     let editor = window.activeTextEditor;
@@ -20,21 +31,21 @@ export module ReportJumper {
     
 
     let currentLineNo = editor.selection.active.line + 1;
-    let onAfterGetRecLineNo = 1;
+    let foundLineNo = 1;
     for (let i = currentLineNo; i < editor.document.lineCount; i++) {
         let currLine = editor.document.lineAt(i);
         let currLineText = currLine.text.toUpperCase();
         currLineText = currLineText.trimLeft();
         currLineText = currLineText.substr(0, text.length);
         if (currLineText.indexOf(text) > -1) {
-            onAfterGetRecLineNo = i;
+            foundLineNo = i;
             break;
         }
          
     }
 
-    if (editor && onAfterGetRecLineNo >= 0) {
-        let range = editor.document.lineAt(onAfterGetRecLineNo).range;
+    if (editor && foundLineNo >= 0) {
+        let range = editor.document.lineAt(foundLineNo).range;
         editor.selection = new Selection(range.end, range.end);
         let revealRange = new Range(new Position(range.end.line - 10, 0), new Position(range.end.line + 10, 0));
         editor.revealRange(revealRange);
